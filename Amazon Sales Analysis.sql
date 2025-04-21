048b929fa72615f4b7fd74700fb64f52ffea407c
ChieNwosu <JosephCNwosu@yahoo.com>
2025-04-21 15:50:47 -0400

    Amazon Sales Analysis.sql

--- Amazon Sales Analysis.sql
+++ Amazon Sales Analysis.sql
@@ -1,4 +1,34 @@
--- Table Statement of the first 10 entries
+-- ####### Amazon Sales Data Analysis ######
+-- Overview of Dataset --
+-- The data consists of sales record of over 1K+ Amazon Product's Ratings and Reviews --
+-- as per their details listed on the official website of Amazon --
+
+-- Objective of Project --
+-- The major aim of this project is to gain insight into the sales data of Amazon --
+-- and to understand the different factors that affect sales of the different  --
+-------------------------------------------------------------------------------------------------------
+
+-- Data Wrangling--
+CREATE TABLE amazon (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_name VARCHAR(255),
+    category VARCHAR(100),
+    discounted_price DECIMAL(10, 2),
+    actual_price DECIMAL(10, 2),
+    discount_percentage DECIMAL(5, 2),
+    rating DECIMAL(3, 1),
+    rating_count INT,
+    about_product TEXT,
+    user_id VARCHAR(50),
+    user_name VARCHAR(100),
+    review_id VARCHAR(50),
+    review_title VARCHAR(255),
+    review_content TEXT,
+    img_link VARCHAR(255),
+    product_link VARCHAR(255)
+);
+
+-- Table Statement
 SELECT * FROM amazon
 LIMIT 10;
 -- This query displays the first 10 rows from the amazon table to preview the data structure and contents
@@ -200,3 +230,39 @@ WHERE
     -- Ensures we only include products with valid prices and discounts
     AND discounted_price > avg_price_cte.avg_price;
     -- Only includes products priced higher than the overall average
+
+-------------------------------------------------------------------------------------------------------
+
+-- This project presents a comprehensive analysis of Amazon sales data using SQL queries --
+-- The analysis extracts key findings about product performance, store operations, customer behavior, and sales patterns. --
+
+-- ##### Key Findings ##### --
+
+-- ##### Product Analysis #####  --
+
+-- Highest Sales Product Line: Electronic Accessories (Units Sold: 971) --
+-- Highest Revenue Product Line: Food and Beverages ($ 56,144.96)-- 
+-- Lowest Sales Product Line: Health and Beauty (Units Sold: 854) -- 
+-- Lowest Revenue Product Line: Health and Beauty ($ 49,193.84) -- 
+
+-- ##### Sales Analysis #####  --
+
+-- Month With Highest Revenue: January ($ 116,292.11) --
+-- City & Branch With Highest Revenue: Naypyitaw[C] ($ 110,568.86) -- 
+-- Month With Lowest Revenue: February ($ 97,219.58) -- 
+-- City & Branch With Lowest Revenue: Mandalay[B] ($ 106,198.00) --
+-- Peak Sales Time: Afternoon --
+-- Peak Sales Day: Saturday --
+
+-- ##### Customer Analysis #####  --
+
+-- Most Predominant Gender: Female -- 
+-- Most Predominant Customer Type: Member --
+-- Highest Revenue Gender: Female ($ 167,883.26) --
+-- Highest Revenue Customer Type: Member ($ 164,223.81) --
+-- Most Popular Product Line (Male): Health and Beauty --
+-- Most Popular Product Line (Female): Fashion Accessories --
+-- Distribution Of Members: Male (240) Female (261) --
+-- Sales by Gender: Male (2,641 units) Female (2,869 units) --
+-------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------
